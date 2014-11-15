72a9fa4b90b06b9e40f828f65f5456860c501b31
Jonathan Claggett <jonathan@claggett.org>
2014-11-15 09:18:38 -0500

    Add initial test-check code

--- test/game2048/core_test.clj
+++ test/game2048/core_test.clj
@@ -1,7 +1,27 @@
 (ns game2048.core-test
-  (:require [clojure.test :refer :all]
+  (:require [clojure.test.check.clojure-test :refer [defspec]]
+            [clojure.test.check.generators :as gen]
+            [clojure.test.check.properties :as prop]
             [game2048.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(def gen-idx (gen/choose 0 15))
+
+(defspec left-works
+  10
+  (prop/for-all [idx gen-idx]
+                (or (nil? (left idx)) (= (left idx) (dec idx)))))
+
+(defspec right-works
+  10
+  (prop/for-all [idx gen-idx]
+                (or (nil? (right idx)) (= (right idx) (inc idx)))))
+
+(defspec up-works
+  10
+  (prop/for-all [idx gen-idx]
+                (or (nil? (up idx)) (= (up idx) (- idx 4)))))
+
+(defspec down-works
+  10
+  (prop/for-all [idx gen-idx]
+                (or (nil? (down idx)) (= (down idx) (+ idx 4)))))
